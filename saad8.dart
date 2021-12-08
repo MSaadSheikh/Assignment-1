6ed632c3b53a0a65df09459208a2cb722581e335
MSaadSheikh <95358942+MSaadSheikh@users.noreply.github.com>
2021-12-08 02:01:25 -0800

    Add files via upload
    First Assignment Done

---
+++ saad8.dart
@@ -0,0 +1,116 @@
+//programe 1
+
+//void main() {
+//var v1 = "saad";
+//var v2 = "watches";
+//int v3 = 6;
+//print("$v1 order $v2 from $v3 rolex store");
+//}
+
+//void main() {
+//var v1 = "Saad";
+//var v2 = "watces";
+//int v3 = 6;
+//print("$v1 order $v2 watces from XYZ Store);
+// }
+
+//programe 2.
+
+import 'dart:io';
+
+//void main() {
+//stdout.write("number is 1 is:");
+//var data = stdin.readLineSync();
+//var v1 = int.parse(data ?? "");
+//stdout.write("number is 2 is:");
+//var data1 = stdin.readLineSync();
+//var v2 = int.parse(data1 ?? "");
+//var v3 = v1 + v2;
+//print(v3);
+//}
+
+//programe 3.
+
+//void main() {
+//stdout.writeln("write first value");
+//int a = int.parse(stdin.readLineSync()!);
+
+//stdout.writeln("write second value");
+//int v1 = int.parse(stdin.readLineSync()!);
+//stdout.writeln();
+//print("subtraction ${a - v1}");
+//}
+
+//programe 4.
+
+//void main() {
+//int cost = 600;
+//stdout.write("Ticket price is : 600\n ");
+//stdout.write("Ticket quantity is : ");
+//int? Tic = int.parse(stdin.readLineSync()!);
+//var t = Tic * cost;
+//print("Total Amount is $t");
+//}
+
+//Programe 5.
+
+//void main() {
+//stdout.write("Enter the current temperature :");
+//int? Temp = int.parse(stdin.readLineSync()!);
+//stdout.write("Recent Temperature is :$Temp C \n ");
+//var F = (Temp * 9 / 5) + 32;
+//print("The Value of Formula is : $F");
+//}
+
+//Program 6.
+
+//void main() {
+//stdout.write("Shopping Cart \n");
+//stdout.write("First item of price is : ");
+//int? price = int.parse(stdin.readLineSync()!);
+//stdout.write("First item Quantity is : ");
+//int? qua2 = int.parse(stdin.readLineSync()!);
+//print(qua2);
+
+//stdout.write("second item of price : ");
+//int? price2 = int.parse(stdin.readLineSync()!);
+//stdout.write("second item quantity is : ");
+//int? qua3 = int.parse(stdin.readLineSync()!);
+//var s = price * qua2 + price2 * qua3;
+//print(s);
+//}
+
+//Program 7.
+
+//void main() {
+//stdout..write("total marks is : ");
+//int tm = int.parse(stdin.readLineSync()!);
+//stdout.write("Obtained marks is : ");
+//int? om = int.parse(stdin.readLineSync()!);
+//var a = om / tm * 100;
+//print(a);
+//}
+
+//Program 8.
+
+//void main() {
+//int usd = 10;
+//int sr = 25;
+//double pkr = 175.70;
+//double rup = 46.85;
+//num paisa = usd * pkr;
+//stdout.write("10 DOLLAR convert in pkr is $paisa \n");
+//num paisa1 = sr * pkr;
+//stdout.write("25 Riyal convert in pkr is $paisa\n");
+//}
+
+//Pogram 9.
+
+//void main() {
+  //stdout.write("enter Hours : ");
+  //int? Hour = int.parse(stdin.readLineSync()!);
+  //stdout.write("Enter rate pr Hour : ");
+  //int? rate = int.parse(stdin.readLineSync()!);
+  //int week = Hour * rate;
+  //stdout.write("your weekly earning is :$week ");
+//}
